306cf4508384762138a7ee88186ab2b0be453d0a
Erick Llanos <erick.llanos.clase@gmail.com>
2023-04-29 18:12:00 +0200

    Cambio rapidinho

--- BudgetBuddyFinal/resources/js/Layouts/AuthenticatedLayout.vue
+++ BudgetBuddyFinal/resources/js/Layouts/AuthenticatedLayout.vue
@@ -13,7 +13,7 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div>
         <div class="min-h-screen bg-gray-100">
-            <nav class="bg-sky-100 border-b border-gray-100">
+            <nav class="bg-[#d8eefe] border-b border-gray-100 w-full shadow-md">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
