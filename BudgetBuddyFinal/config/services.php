a6d425967820eb6c04445805dc80c9b490d68554
Erick Llanos <erick.llanos.clase@gmail.com>
2023-05-07 17:18:27 +0200

    Paquete Socialite agregado; google funciona, github hay que ver errores.

--- BudgetBuddyFinal/config/services.php
+++ BudgetBuddyFinal/config/services.php
@@ -30,5 +30,15 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_OAUTH_ID'),
+        'client_secret' => env('GOOGLE_OAUTH_SECRET'),
+        'redirect' => '/google-callback',
+    ],
+    'github' => [
+        'client_id' => env('GITHUB_OAUTH_ID'),
+        'client_secret' => env('GITHUB_OAUTH_SECRET'),
+        'redirect' => '/github-callback',
+    ],
 
 ];
--- BudgetBuddyFinal/routes/web.php
+++ BudgetBuddyFinal/routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
 use Inertia\Inertia;
 
 /*
@@ -61,6 +63,71 @@ Route::get('/contact', function(){
 });
 Route::get('/blog', function(){
     return Inertia::render('Blog');
+});
+
+// AUTHENTICATION USING LARAVEL SOCIALITE
+Route::get('/login-google', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/google-callback', function () {
+    $user = Socialite::driver('google')->user();
+    // dd($user);
+    // Usamos el campo id que acabamos de crear
+    $userExists = User::where('external_id', $user->id)->where('external_auth', 'google')->first();
+    // Consulta a la base de datos para buscar un registro en la tabla "User" que tenga un valor "external_id" igual a $user->id y un valor "external_auth" igual a "google"
+
+    // "first" se utiliza para obtener el primer registro que cumpla con las condiciones
+
+    // Si un registro coincide con los valores proporcionados, se almacenará en la variable "$userExists"
+
+    if ($userExists) {
+        // Si existe el usuario, hacemos login
+        Auth::login($userExists);
+        # code...
+    } else {
+        // Si no, creamos el usuario
+        $userNew = User::create([
+            'name'=>$user->name,
+            'email'=>$user->email,
+            'avatar'=>$user->avatar,
+            'external_id'=>$user->id,
+            'external_auth'=>'google'
+        ]);
+        Auth::login($userNew);
+    }
+    return redirect('/dashboard');
+    // $user->token
+});
+
+// GITHUB
+Route::get('/login-github', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/github-callback', function () {
+    $user = Socialite::driver('github')->user();
+    $userExists = User::where('external_id', $user->id)->where('external_auth', 'github')->first();
+
+    if ($userExists) {
+        // Si existe el usuario, hacemos login
+        Auth::login($userExists);
+        # code...
+    } else {
+        $userNew = User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'avatar' => $user->avatar,
+            'external_id' => $user->id,
+            'external_auth' => 'github'
+        ]);
+        Auth::login($userNew);
+    }
+
+    // dd($user);
+
+    // $user->token
+    return redirect('/dashboard');
 });
 
 require __DIR__.'/auth.php';
